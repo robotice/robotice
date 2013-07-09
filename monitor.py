aff5d9a4eba430500ce28cde3bf5232b64bd415f
majklcz <6du1ro.n@gmail.com>
2013-07-09 22:09:38 +0200

    statsd

--- monitor.py
+++ monitor.py
@@ -5,42 +5,36 @@ import re
 import sys
 import time
 import datetime
-import gspread
+import python-statsd
 
-# ===========================================================================
-# Google Account Details
-# ===========================================================================
+# Open a connection to `server` on port `8125` with a `50%` sample rate
+statsd_connection = statsd.Connection(
+    host='server',
+    port=8125 ,
+    sample_rate=0.5,
+)
+ # Create a client for this application
+statsd_client = statsd.Client("temp&humidity", statsd_connection)
 
-# Account details for google docs
-email       = 'you@somewhere.com'
-password    = '$hhh!'
-spreadsheet = 'SpreadsheetName'
+class Monitor(object):
+     def __init__(self):
+        # Create a client specific for this class
+        self.statsd_client = statsd_client.get_client(
+        self.__class__.__name__)
 
-# ===========================================================================
-# Example Code
-# ===========================================================================
-
-
-# Login with your Google account
-try:
-  gc = gspread.login(email, password)
-except:
-  print "Unable to log in.  Check your email address/password"
-  sys.exit()
-
-# Open a worksheet from your spreadsheet using the filename
-try:
-  worksheet = gc.open(spreadsheet).sheet1
-  # Alternatively, open a spreadsheet using the spreadsheet's key
-  # worksheet = gc.open_by_key('0BmgG6nO_6dprdS1MN3d3MkdPa142WFRrdnRRUWl1UFE')
-except:
-  print "Unable to open the spreadsheet.  Check your filename: %s" % spreadsheet
-  sys.exit()
+     def do_something(self):
+        # Create a `timer` client
+        timer = self.statsd_client.get_client(class_=statsd.Timer)
+        # start the measurement
+        timer.start()
+        # do something
+        timer.interval('intermediate_value')
+        # do something else
+        timer.stop('total')
 
 # Continuously append data
 while(True):
   # Run the DHT program to get the humidity and temperature readings!
-
   output = subprocess.check_output(["./Adafruit_DHT", "2302", "4"]);
   print output
   matches = re.search("Temp =\s+([0-9.]+)", output)
@@ -58,15 +52,5 @@ while(True):
 
   print "Temperature: %.1f C" % temp
   print "Humidity:    %.1f %%" % humidity
- 
-  # Append the data in the spreadsheet, including a timestamp
-  try:
-    values = [datetime.datetime.now(), temp, humidity]
-    worksheet.append_row(values)
-  except:
-    print "Unable to append data.  Check your connection?"
-    sys.exit()
 
-  # Wait 30 seconds before continuing
-  print "Wrote a row to %s" % spreadsheet
-  time.sleep(30)
+  values = [datetime.datetime.now(), temp, humidity]
