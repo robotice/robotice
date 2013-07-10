35feb7bf3b077120f1730f74c98dce86c7fdab58
newt <mail@newt.cz>
2013-07-10 20:14:18 +0200

    test sending data

--- sensors/dht.py
+++ sensors/dht.py
@@ -10,24 +10,28 @@ def get_dht_data(sensor):
   """
   Run the DHT program to get the humidity and temperature readings!
   """
-  executable = "/srv/robotice/sensors/dht/Adafruit_DHT"
+  executable = "/srv/robotice/sensors/Adafruit_DHT"
   version = str(sensor.get('version'))
   port = str(sensor.get('port'))
   output = subprocess.check_output([executable, version, port]);
   print output
   matches = re.search("Temp =\s+([0-9.]+)", output)
   if (not matches):
-    time.sleep(3)
-  temp = float(matches.group(1))
+    temp = None
+  else:
+    temp = float(matches.group(1))
   
-  # search for humidity printout
   matches = re.search("Hum =\s+([0-9.]+)", output)
   if (not matches):
-    time.sleep(3)
-  humidity = float(matches.group(1))
+    humidity = None
+  else:
+    humidity = float(matches.group(1))
 
   print "Temperature: %.1f C" % temp
   print "Humidity:    %.1f %%" % humidity
  
-  values = [datetime.datetime.now(), temp, humidity]
+  values = [
+    [datetime.datetime.now(), 'dht.0.temperature', temp],
+    [datetime.datetime.now(), 'dht.0.humidity', humidity]
+  ]
   return values
---
+++ simple_monitor.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import subprocess
+import re
+import sys
+import time
+import datetime
+import statsd
+
+from sensors.dht import get_dht_data
+
+DUMMY_SENSOR = {
+    'version': 2302,
+    'port': 4
+}
+
+# Open a connection to `server` on port `8125` with a `50%` sample rate
+statsd_connection = statsd.Connection(
+    host='master2.htfs.info',
+    port=8125 ,
+    sample_rate=0.5,
+    disabled = False
+)
+
+data = get_dht_data(DUMMY_SENSOR)
+
+print data
+ # Create a client for this application
+statsd_client = statsd.Client("temp&humidity", statsd_connection)
+
+print statsd_client
+
+for datum in data:
+  gauge.send(datum[1], datum[2])
