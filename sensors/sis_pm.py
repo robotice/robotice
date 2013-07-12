fa9fb75ef45aa8b8c6c35801912620bf496c9942
majklcz <6du1ro.n@gmail.com>
2013-07-12 21:41:39 +0200

    sispm

---
+++ sensors/sis_pm.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import subprocess
+import re
+import sys
+import time
+import yaml
+
+config_file = open("/srv/robotice/config.yml", "r")
+
+config = yaml.load(config_file)
+
+def get_sispm_data(device):
+  """
+  sispcml
+  """
+  executable = "/usr/local/bin/sispmctl"
+  output = subprocess.check_output([executable, "-m", "all"]);
+
+  if config.get("debug"):
+    print output
+
+  return output
