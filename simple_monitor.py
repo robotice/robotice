1b2b9ba9cd1d4a14ec11cb0c1adb5caa2d604931
majklcz <6du1ro.n@gmail.com>
2013-07-12 20:53:36 +0200

    fixes

--- simple_monitor.py
+++ simple_monitor.py
@@ -14,7 +14,7 @@ config_file = open("/srv/robotice/config.yml", "r")
 
 config = yaml.load(config_file)
 
-if config.get("debug") == True
+if config.get("debug"):
 	print config
 
 # Open a connection to `server` on port `8125` with a `50%` sample rate
@@ -36,7 +36,7 @@ while True:
 	for sensor in config.get("sensors"):
 		if sensor.get("type") == "dht":
 			data = get_dht_data(sensor)
-	if data == null
+	if data == None:
 		break
   	gauge.send(data[1], data[2])
 	time.sleep(2)
