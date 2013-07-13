6f26bf1e78c19f72de458d608ad948274e95e3b8
majklcz <6du1ro.n@gmail.com>
2013-07-13 22:44:22 +0200

    add light sensor

---
+++ simple_light_sensor.py
@@ -0,0 +1,14 @@
+import Adafruit_BBIO.ADC as ADC
+import time
+ 
+sensor_pin = 'P9_40'
+ 
+ADC.setup()
+ 
+print('Reading\t\tVolts')
+ 
+while True:
+    reading = ADC.read(sensor_pin)
+    volts = reading * 1.800
+    print('%f\t%f' % (reading, volts))
+    time.sleep(1)
