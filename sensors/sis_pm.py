ead452eb9703c00efcddedc71bb29ef9dba33c22
majklcz <6du1ro.n@gmail.com>
2013-07-13 08:19:56 +0200

    change get_sispm_data

--- sensors/sis_pm.py
+++ sensors/sis_pm.py
@@ -14,9 +14,11 @@ def get_sispm_data():
   sispcml
   """
   executable = "/usr/local/bin/sispmctl"
-  output = subprocess.check_output([executable, "-m", "all"]);
+  values = []
+  for socket in xrange(1,4):
+    output = subprocess.check_output([executable, "-m", socket]);
+    values[socket] = [timestamp = int(time.time()),"sispm.0.{socket}", output]
+    if config.get("debug"):
+      print values[socket]
 
-  if config.get("debug"):
-    print output
-
-  return output
+  return values
