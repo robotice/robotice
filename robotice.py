ce10d066e083e6ccab3513935d2f4068873f9f21
majklcz <6du1ro.n@gmail.com>
2013-07-14 19:10:49 +0200

    new robotice

--- robotice.py
+++ robotice.py
@@ -1,12 +1,19 @@
 import logging
 from daemonize import Daemonize
 from time import sleep
+from simple_monitor import send_data_all
 
-pid = "/tmp/robtice.pid"
-logger = logging.getLogger("robotice")
+config_file = open("/srv/robotice/config.yml", "r")
+
+config = yaml.load(config_file)
+
+name = config.get("name")
+
+pid = "/tmp/%s.pid" %name
+logger = logging.getLogger("%s" %name)
 logger.setLevel(logging.DEBUG)
 logger.propagate = False
-fh = logging.FileHandler("/tmp/robotice.log", "w")
+fh = logging.FileHandler("/tmp/%s.log" %name, "w")
 fh.setLevel(logging.DEBUG)
 logger.addHandler(fh)
 keep_fds = [fh.stream.fileno()]
@@ -15,8 +22,9 @@ def main():
     while True:
         print "ahoj"
         logger.debug("test")
+        #send_data_all(logger,conf,sender)
         sleep(2)
         print "ahoj2"
 
-daemon = Daemonize(app="robotice", pid=pid, action=main,keep_fds=keep_fds) 
+daemon = Daemonize(app="%s" %name, pid=pid, action=main,keep_fds=keep_fds) 
 daemon.start()
