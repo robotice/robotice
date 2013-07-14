118b511cf635ba49b7711dc9bd29bf0312b552ee
majklcz <6du1ro.n@gmail.com>
2013-07-14 19:01:00 +0200

    daemonize

--- robotice.py
+++ robotice.py
@@ -1,43 +1,22 @@
-#!/usr/bin/env python
+import logging
+from daemonize import Daemonize
+from time import sleep
 
-"""
-Robotice daemon
-based on http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
-"""
+pid = "/tmp/robtice.pid"
+logger = logging.getLogger("robotice")
+logger.setLevel(logging.DEBUG)
+logger.propagate = False
+fh = logging.FileHandler("/tmp/robotice.log", "w")
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+keep_fds = [fh.stream.fileno()]
 
-import sys, time
+def main():
+    while True:
+        print "ahoj"
+        logger.debug("test")
+        sleep(2)
+        print "ahoj2"
 
-from daemon import Daemon
-
-from sensors.dht import get_dht_data
-from outputs.statsd import send_statsd_data
-
-DUMMY_SENSOR = {
-    'version': 2302,
-    'port': 4
-}
-
-class Robotice(Daemon):
-    def run(self):
-        while True:
-            data = get_dht_data(DUMMY_SENSOR)
-            send_statsd_data(data)
-            time.sleep(2)
-            print 'sleeep...'
- 
-if __name__ == "__main__":
-    daemon = Robotice('/tmp/robotice.pid')
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
-        else:
-            print "Unknown command"
-            sys.exit(2)
-        sys.exit(0)
-    else:
-        print "usage: %s start|stop|restart" % sys.argv[0]
-        sys.exit(2)
+daemon = Daemonize(app="robotice", pid=pid, action=main,keep_fds=keep_fds) 
+daemon.start()
--- simple_daemon.py
+++ simple_daemon.py
@@ -1,38 +1,35 @@
 #!/usr/bin/python
 
-import subprocess
-import re
-import sys
-import statsd
-import yaml
 import undead
 
-from time import sleep
-from simple_monitor import send_data_all
+#from time import sleep
+#from simple_monitor import send_data_all
 
 undead.name = "robotice-daemon"
 undead.proc_title = "robotice"
 undead.settings["pidfile"] = "/tmp/robotice.pid"
 
 @undead
-def robotice(log):
-	log.info("start initial config")
-	config_file = open("/srv/robotice/config.yml", "r")
-	config = yaml.load(config_file)
+def robotice_method(log):
+	log.info("start initial config2")
+#	config_file = open("/srv/robotice/config.yml", "r")
+#	config = yaml.load(config_file)
 
 	#add additional settings
-	statsd_connection = statsd.Connection(
-	    host=config.get("statsd").get("host"),
-	    port=config.get("statsd").get("port"),
-	    sample_rate=1,
-	    disabled = False
-	)
-	log.info("statsd config %s" %statsd_connection)
+#	statsd_connection = statsd.Connection(
+#	    host=config.get("statsd").get("host"),
+#	    port=config.get("statsd").get("port"),
+#	    sample_rate=1,
+#	    disabled = False
+#	)
+#	log.info("statsd config %s" %statsd_connection)
 	# Create a client for this application
 	#statsd_client = statsd.Client("temp&humidity", statsd_connection)
-	sender = statsd.Gauge('robotice_prod.%s' %config.get("name").replace(".","_"), statsd_connection)
+#	sender = statsd.Gauge('robotice_prod.%s' %config.get("name").replace(".", "_"), statsd_connection)
 	#log.info("daemon: %s \n pidfile: %s" %undead.proc_title %undead.settings["pidfile"])
 	while True:
-		send_data_all(log,config,sender)
-		sleep(2)
+#		send_data_all(log,config,sender)
+#		log.info("cycle")
+#		sleep(2)
+		pass
 
