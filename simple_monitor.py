c5036cbf68b5a1d59d7918ad463b629a23bf9803
majklcz <6du1ro.n@gmail.com>
2013-07-14 12:56:28 +0200

    undead

--- simple_monitor.py
+++ simple_monitor.py
@@ -7,6 +7,7 @@ import time
 import datetime
 import statsd
 import yaml
+import undead
 
 from sensors.dht import get_dht_data
 from sensors.sis_pm import get_sispm_data
@@ -14,6 +15,10 @@ from sensors.sis_pm import get_sispm_data
 config_file = open("/srv/robotice/config.yml", "r")
 
 config = yaml.load(config_file)
+
+#damon setting
+undead.name = "simple-monitor-daemon"
+undead.process_name = "monitordaemon"
 
 # Open a connection to `server` on port `8125` with a `50%` sample rate
 statsd_connection = statsd.Connection(
@@ -36,13 +41,15 @@ def send_data(data):
 			print datum
 			gauge.send(datum[1], datum[2])
 
-while True:
-	for sensor in config.get("sensors"):
-		if sensor.get("type") == "dht":
-			data = get_dht_data(sensor)
-  			send_data(data)
-  		elif sensor.get("type") == "sispm":
-			data = get_sispm_data()
-			send_data(data)
+@undead
+def deamon_test():
+	while True:
+		for sensor in config.get("sensors"):
+			if sensor.get("type") == "dht":
+				data = get_dht_data(sensor)
+  				send_data(data)
+  			elif sensor.get("type") == "sispm":
+				data = get_sispm_data()
+				send_data(data)
   			
-	time.sleep(2)
+		time.sleep(2)
