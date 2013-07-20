9a1a6a2617b21b1fe89eefd249b96ed093f7e5fe
newt <mail@newt.cz>
2013-07-20 14:57:54 +0200

    sleep correctly

--- actuators/sispm.py
+++ actuators/sispm.py
@@ -12,13 +12,14 @@ executable = "/usr/local/bin/sispmctl"
 def run_action(device, delay):
   on_command = [executable, "-d", str(device.get('device')), '-o', str(device.get('socket'))]
   off_command = [executable, "-d", str(device.get('device')), '-f', str(device.get('socket'))]
+
   try:
     output = subprocess.check_output(on_command);
   except Exception, e:
     print e
     return None
 
-  time.wait(delay)
+  time.sleep(delay)
 
   try:
     output = subprocess.check_output(off_command);
