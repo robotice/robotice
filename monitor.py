16ab55875905c02a4ba89b02c0f5c5709a02875c
newt <mail@newt.cz>
2013-07-22 11:23:35 +0200

    new script files

--- monitor.py
+++ monitor.py
@@ -1,3 +1,7 @@
+from celery import Celery
+
 BROKER_URL = "amqp://celery:celery@master2.htfs.info:5672//celery"
 CELERY_RESULT_BACKEND = "amqp"
-CELERY_IMPORTS = ("tasks.monitor", "tasks.reactor", "tasks.planner")
+CELERY_IMPORTS = ("tasks.monitor",)
+
+celery = Celery(broker=BROKER_URL)
--- robotice-monitor.py
+++
@@ -1,4 +0,0 @@
-
-from celery.task import task
- 
-from tasks.monitor import get_data
