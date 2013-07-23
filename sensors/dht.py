cbb4c5f730a44211374a7d2ed9a1b66bdf3ec5ad
newt <mail@newt.cz>
2013-07-23 17:20:56 +0200

    sensors fixes

--- sensors/dht.py
+++ sensors/dht.py
@@ -3,43 +3,39 @@
 import subprocess
 import re
 import sys
-import yaml
 
-config_file = open("/srv/robotice/config.yml", "r")
-
-config = yaml.load(config_file)
+executable = "/srv/robotice/sensors/Adafruit_DHT"
 
 def get_dht_data(sensor):
-  """
-  Run the DHT program to get the humidity and temperature readings!
-  """
-  executable = "/srv/robotice/sensors/Adafruit_DHT"
-  version = str(sensor.get('version'))
-  port = str(sensor.get('port'))
-  output = subprocess.check_output([executable, version, port]);
-  #print output
-  matches = re.search("Temp =\s+([0-9.]+)", output)
-  if (not matches):
-    temp = None
-  else:
-    temp = float(matches.group(1))
-  
-  matches = re.search("Hum =\s+([0-9.]+)", output)
-  if (not matches):
-    humidity = None
-  else:
-    humidity = float(matches.group(1))
+    """
+    Run the DHT program to get the humidity and temperature readings!
+    """
+    device = sensor.get('device'), 0)
+    version = sensor.get('version')
+    port = sensor.get('port'))
 
-  if temp == None or humidity == None:
-    return None
+    output = subprocess.check_output([executable, str(version), str(port)]);
 
-  if config.get("debug"):
-    print "Temperature: %.1f C" % temp
-    print "Humidity:    %.1f %%" % humidity
+    matches = re.search("Temp =\s+([0-9.]+)", output)
+    if (not matches):
+       temp = None
+    else:
+        temp = float(matches.group(1))
 
-  values = [
-    ['dht.%s.temperature' % port, temp],
-    ['dht.%s.humidity' % port, humidity]
-  ]
-  return values
+    matches = re.search("Hum =\s+([0-9.]+)", output)
+    if (not matches):
+        humidity = None
+    else:
+        humidity = float(matches.group(1))
 
+    if temp == None or humidity == None:
+        values = [
+            ['dht.%s.temperature' % device, None],
+            ['dht.%s.humidity' % device, None]
+        ]
+    else:
+        values = [
+            ['dht.%s.temperature' % device, temp],
+            ['dht.%s.humidity' % device, humidity]
+        ]
+    return values
