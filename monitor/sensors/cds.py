7b01ed549d47dfc4237007a111cff91fb73c365f
newt <mail@newt.cz>
2013-07-27 21:52:42 +0200

    cds driver call fix

--- monitor/sensors/cds.py
+++ monitor/sensors/cds.py
@@ -4,7 +4,7 @@ executable = "/srv/robotice/sensors/cds/driver.py"
 
 def get_cds_data(sensor):
 
-    value = subprocess.check_output([executable, str(sensor.get('port')), sensor.get('cpu_arch')])
+    value = subprocess.check_output([executable, '-p', str(sensor.get('port')), '-a', sensor.get('cpu_arch')])
 
     data = []
     data.append(["cds.%s.luminosity" % sensor.get('device'), int(value)])
