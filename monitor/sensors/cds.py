2258dbf35af6d250c7e0edffd9634eaa1c927d0a
newt <mail@newt.cz>
2013-07-27 19:51:40 +0200

    fake sensor

--- monitor/sensors/cds.py
+++ monitor/sensors/cds.py
@@ -1,27 +1,11 @@
 #!/usr/bin/python
-import argparse
+import subprocess
 
-p = argparse.ArgumentParser(description="Parse command arameters.")
-
-p.add_argument("-p", "--port")
-p.add_argument("-a", "--arch")
-
-opts = p.parse_args()
+executable = "/srv/robotice/sensors/cds/cds.py"
 
 def get_cds_data(sensor):
-    if device = "rpi":
-      import Adafruit_BBIO.ADC as ADC
 
-	    ADC.setup()
-	    reading = ADC.read(opts.port)
+    data = []
+    data.append( ["cds.%s.luminosity" % sensor.get('device'), 40] )
 
-    if device = "beagle":
-	    import RPi.GPIO as GPIO
-
-	    GPIO.setmode(GPIO.BCM)
-
-	    GPIO.setup(opts.port, GPIO.IN)
-      reading = GPIO.input(opts.port)
-	    GPIO.cleanup()
-      
-    return reading
+    return data
