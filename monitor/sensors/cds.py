6977f3c06160e182d70c658cc8d8ba9140c5d405
newt <mail@newt.cz>
2013-07-27 21:18:04 +0200

    cleanup

--- monitor/sensors/cds.py
+++ monitor/sensors/cds.py
@@ -5,9 +5,9 @@ executable = "/srv/robotice/sensors/cds/cds_driver.py"
 
 def get_cds_data(sensor):
 
-    value = subprocess.check_output([executable, str(sensor.get('port')), sensor.get('cpu_arch')]);
+    value = subprocess.check_output([executable, str(sensor.get('port')), sensor.get('cpu_arch')])
 
     data = []
-    data.append( ["cds.%s.luminosity" % sensor.get('device'), int(value)] )
+    data.append(["cds.%s.luminosity" % sensor.get('device'), int(value)])
 
     return data
