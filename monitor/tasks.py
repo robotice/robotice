c54d91d1892f2749fcc921338b5eb3251f3a9160
newt <mail@newt.cz>
2013-07-27 23:27:27 +0200

    )

--- monitor/tasks.py
+++ monitor/tasks.py
@@ -42,7 +42,7 @@ def return_real_data(results, config):
 
     for result in results.join():
         for datum in result:
-            if isinstance(datum[1], (int, long, float, decimal.Decimal):
+            if isinstance(datum[1], (int, long, float, decimal.Decimal)):
                 task_results.append(datum)
                 metering.send(datum[0], datum[1])
                 database.set('%s.%s' % (config.metering_prefix, datum[0]), datum[1])
