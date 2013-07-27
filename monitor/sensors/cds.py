556decba627b2d78c1dc12ff730f23b558f2ac62
majklcz <6du1ro.n@gmail.com>
2013-07-27 21:08:32 +0200

    cds changes

--- monitor/sensors/cds.py
+++ monitor/sensors/cds.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 import subprocess
 
-executable = "/srv/robotice/sensors/cds/cds.py"
+executable = "/srv/robotice/sensors/cds/cds_driver.py"
 
 def get_cds_data(sensor):
 
-    data = []
-    data.append( ["cds.%s.luminosity" % sensor.get('device'), 40] )
+	value = subprocess.check_output([executable, str(port), str(arch)]);
+
+	data = []
+    data.append( ["cds.%s.luminosity" % sensor.get('device'), int(value)] )
 
     return data
