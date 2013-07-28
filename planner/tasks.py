2db8cf1b49423f41aaff980066746620ac796232
newt <mail@newt.cz>
2013-07-28 20:53:20 +0200

    planner fix

--- planner/tasks.py
+++ planner/tasks.py
@@ -1,8 +1,6 @@
 from yaml import load
 from celery.task import task
 
-logger = get_task_logger(__name__)
-
 @task(name='planner.get_model_data')
 def get_model_data(config):
     return config.plans
