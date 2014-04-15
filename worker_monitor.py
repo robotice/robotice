b5978475de6f537088664784fd3d6a92299888dc
Michael Kutý <6du1ro.n@gmail.com>
2014-04-15 16:11:20 +0200

    reasoner  changes

--- worker_monitor.py
+++ worker_monitor.py
@@ -24,14 +24,18 @@ monitor_exchange = Exchange('monitor', type='fanout')
 monitor_local_exchange = Exchange('monitor_%s' % config.hostname, type='fanout')
 reactor_exchange = Exchange('reactor', type='fanout')
 planner_exchange = Exchange('planner', type='fanout')
+reasoner_exchange = Exchange('reasoner', type='fanout')
 
 CELERY_ACCEPT_CONTENT = ['json', 'msgpack', 'yaml','application/x-python-serialize',]
+
+CELERY_REDIRECT_STDOUTS_LEVEL = "INFO"
 
 CELERY_QUEUES = (
     Queue('default', default_exchange, routing_key='default'),
     Queue('monitor', monitor_exchange, routing_key='monitor.#'),
     Queue('monitor_%s' % config.hostname, monitor_local_exchange, routing_key='monitor_%s.#' % config.hostname),
     Queue('planner', planner_exchange, routing_key='planner.#'),
+    Queue('reasoner', reasoner_exchange, routing_key='reasoner.#'),
 )
 
 CELERY_DEFAULT_QUEUE = 'default'
@@ -42,29 +46,14 @@ CELERY_DEFAULT_ROUTING_KEY = 'default'
 CELERY_TIMEZONE = 'UTC'
 
 CELERY_ROUTES = {
-    'monitor.get_real_data': {
-        'queue': 'monitor',
+    'reasoner.process_real_data': {
+        'queue': 'reasoner',
     },
     'monitor.get_sensor_data': {
         'queue': 'monitor',
     },
-    'monitor.get_sensor_data.cds': {
-        'queue': 'monitor_%s' % config.hostname,
-    },
-    'monitor.get_sensor_data.dht': {
-        'queue': 'monitor_%s' % config.hostname,
-    },
-    'monitor.get_sensor_data.dummy': {
-        'queue': 'monitor_%s' % config.hostname,
-    },
-    'monitor.get_sensor_data.sispm': {
-        'queue': 'monitor_%s' % config.hostname,
-    },
 }
 
 celery = Celery('robotice', broker=BROKER_URL)
-celery.control.time_limit('monitor.get_sensor_data.cds', soft=60, hard=120, reply=True)
-celery.control.time_limit('monitor.get_sensor_data.sispm', soft=60, hard=120, reply=True)
-celery.control.time_limit('monitor.get_sensor_data.dht', soft=60, hard=120, reply=True)
 celery.control.time_limit('monitor.get_sensor_data', soft=60, hard=120, reply=True)
-celery.control.time_limit('monitor.get_real_data', soft=60, hard=120, reply=True)
+#celery.control.time_limit('monitor.get_real_data', soft=60, hard=120, reply=True)
