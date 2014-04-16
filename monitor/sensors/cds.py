12e4ff01c0bf24c87161e7f9288e4d4372fd7cf9
Michael Kutý <6du1ro.n@gmail.com>
2014-04-16 20:03:31 +0200

    cds fix

--- monitor/sensors/cds.py
+++ monitor/sensors/cds.py
@@ -5,8 +5,8 @@ executable = "/srv/robotice/sensors/cds/driver.py"
 def get_data(sensor):
     port = str(sensor.get('port'))
     arch = sensor.get('cpu_arch')
-    mode = sensor.get('mode')
-    value = subprocess.check_output([executable, '-p', port, '-a', arch, '-m', mode])
+
+    value = subprocess.check_output([executable, '-p', str(port), '-a', str(arch)])
 
     data = []
 
