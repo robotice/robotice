2a1952c6d84ea28b74486b702dae876f198ec261
Michael Kuty <6du1ro.n@gmail.com>
2014-04-24 20:38:23 +0200

    get plan implementation

--- planner/tasks.py
+++ planner/tasks.py
@@ -5,6 +5,17 @@ from celery.task import task
 def get_model_data(config):
     return config.plans
 
+def get_plan(config):
+	"""pro dany system vrati plan"""
+	for system in config.systems:
+        for plan in config.config.plans:
+			if system.get("name") == plan.get("name"):
+        		for _plan in config.plans:
+        			if _plan.get("name") == system.get("plan"):
+                		return _plan
+    return None
+
 @task(name='planner.return_model_data')
 def return_model_data(config):
-    return config.plans
+    values = []
+    return get_plan(config)
