e974d017b91a9867dc708ed95b17d7efa75b2bae
Michael Kuty <6du1ro.n@gmail.com>
2014-04-25 16:05:08 +0200

    planer fixes

--- planner/tasks.py
+++ planner/tasks.py
@@ -21,20 +21,23 @@ def get_model_data(config):
     now = datetime.datetime.now()
     plan_cycle = int(plan.get('cycle'))
     for device in devices:
-        try:
-            for cycle in device.get('cycles'):
-                cycle_start = int(cycle.get('start'))#datetime.datetime.strptime(str(cycle.get('start')), "%H:%M:%S")
-                cycle_end = int(cycle.get('end'))#datetime.datetime.strptime(str(cycle.get('end')), "%H:%M:%S")
-            path = "{0}.{1}.{2}".format(system.get('name'), 'actuator', device.get('name'))
-            time_delta = now - start
-            relative = floor(time_delta.seconds / plan_cycle)
-            time_delta_ = relative - relative
-            value = 0
-            if (time_delta_ > cycle_start) and (time_delta_ < cycle_end):
-                value = 1
+        for cycle in device.get('cycles'):
+            cycle_start = int(cycle.get('start'))#datetime.datetime.strptime(str(cycle.get('start')), "%H:%M:%S")
+            cycle_end = int(cycle.get('end'))#datetime.datetime.strptime(str(cycle.get('end')), "%H:%M:%S")
+        path = "{0}.{1}.{2}.{3}".format(system.get('name'), 'actuator', device.get('name'), 'model')
+        time_delta = now - start
+        relative = floor(time_delta.seconds / plan_cycle)
+        time_delta_ = relative - relative
+        value, values = 0, (0, 0)
+        if (time_delta_ > cycle_start) and (time_delta_ < cycle_end):
+            if cycle.has_key('value'):
+                value = cycle.get('value')
+            else:
+                values = (cycle.get('value_low'), cycle.get('value_high'))
+        if cycle.has_key('value'):
             config.database.set(path, value)
-        except Exception, e:
-            pass
+        else:
+            config.database.set(path, values)
     return time_delta
 
 @task(name='planner.return_model_data')
