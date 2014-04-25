925b677994704c0d003feebffcdd2e5febedbdc5
Ales Komarek <mail@newt.cz>
2014-04-26 01:15:57 +0200

    logic fix

--- reactor/actuators/relay.py
+++ reactor/actuators/relay.py
@@ -7,6 +7,7 @@ import logging
 logger = logging.getLogger("robotice")
 logger.setLevel(logging.DEBUG)
 
+python = '/srv/robotice/bin/python2'
 executable = '/srv/robotice/actuators/relay/driver.py'
 
 def run(device, model_data, real_data):
@@ -15,10 +16,20 @@ def run(device, model_data, real_data):
 
   status_file = '/tmp/robotice_%s' % device.get('name')
 
-  if int(model_data) == 0:
-    command = ['python', executable, "-a", device.get('architecture'), '-p', str(device.get('port')), '-m', 'off']
+
+  if device.get('reverse', 'off') == 'on':
+
+    if int(model_data) == 0:
+      command = [python, executable, "-a", device.get('architecture'), '-p', str(device.get('port')), '-m', 'off', '-r', 'on']
+    else:
+      command = [python, executable, "-a", device.get('architecture'), '-p', str(device.get('port')), '-m', 'on', '-r', 'on']
+
   else:
-    command = ['python', executable, "-a", device.get('architecture'), '-p', str(device.get('port')), '-m', 'on']
+
+    if int(model_data) == 0:
+      command = [python, executable, "-a", device.get('architecture'), '-p', str(device.get('port')), '-m', 'off']
+    else:
+      command = [python, executable, "-a", device.get('architecture'), '-p', str(device.get('port')), '-m', 'on']
 
   output = subprocess.check_output(command)
 
