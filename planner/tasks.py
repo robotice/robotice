468d410d3891ae70aa77793766e6dc23f0f5b51d
Michael Kuty <6du1ro.n@gmail.com>
2014-04-25 14:54:35 +0200

    fix import

--- planner/tasks.py
+++ planner/tasks.py
@@ -1,6 +1,5 @@
 from yaml import load
 from celery.task import task
-from dateutil import parser
 import datetime
 
 def get_plan(config):
@@ -19,7 +18,7 @@ def get_model_data(config):
     devices = plan.get('actuators') + plan.get('sensors')
     start = datetime.datetime.strptime(str(system.get('start')), "%Y-%m-%d %H:%M:%S")
     now = datetime.datetime.now()
-
+    plan_cycle = plan.get('cycle')
     for device in devices:
         for cycle in device.get('cycles'):
             cycle_start = int(cycle.get('start'))#datetime.datetime.strptime(str(cycle.get('start')), "%H:%M:%S")
@@ -27,7 +26,8 @@ def get_model_data(config):
             cycle_delta = cycle_end - cycle_start
         path = "{0}.{1}.{2}".format(system.get('name'), 'actuator', device.get('name'))
         time_delta = now - start
-        relative = time_delta / cycle_delta
+        relative = time_delta / plan_cycle
+        
     return time_delta
 
 @task(name='planner.return_model_data')
