adb65d2fb5ce7a36cb158b73a74fbae8bd25ce7d
Michael Kuty <6du1ro.n@gmail.com>
2014-04-25 20:22:45 +0200

    changes

--- reasoner/tasks.py
+++ reasoner/tasks.py
@@ -4,8 +4,10 @@ import decimal
 
 from datetime import datetime 
 from celery.task import task
+from celery import group, chord
 from celery.execute import send_task
 from utils import setup_app
+from reactor.tasks import commit_action
 
 @task(name='reasoner.process_data')
 def process_data(model, data):
@@ -53,25 +55,21 @@ def get_plan(config, device_name, device_metric):
 def get_db_values(config, system, plan_name, type='sensors'):
     """return tuple(model_value, real_value)
     """
-    logger = compare_data.get_logger()
     try:
         system_name = system.get('name')
     except Exception, e:
         return None, None
     db_key_real = '%s.%s.%s.%s' % (system.get('name'), type, plan_name, 'real')
     db_key_model = '%s.%s.%s.%s' % (system.get('name'), type, plan_name, 'model')
-    logger.info(db_key_model)
     model_value = config.database.get(db_key_model)
     real_value = config.database.get(db_key_real)
-    logger.info("key: {0} value: {1}".format(db_key_model, model_value))
-    logger.info("key: {0} value: {1}".format(db_key_real, real_value))
     return model_value, real_value
 
 @task(name='reasoner.compare_data')
 def compare_data(config):
     """
     Core task that runs every 1-60 seconds
-    """
+    """ 
 
     logger = compare_data.get_logger()
     
@@ -79,20 +77,21 @@ def compare_data(config):
     logger.info('Compare data started {0}'.format(now))
 
     results = []
-    #tasks = []
+    tasks = []
 
     for sensor in config.sensors:
         system, plan_name = get_plan(config, sensor.get('name'), sensor.get("metric"))
         model_value, real_value = get_db_values(config, system, plan_name)
+        logger.info("model_value: {0} | real_value: {1}".format(model_value, real_value))
         if model_value != real_value:
             #tasks.append(.subtask((config, sensor, grains), exchange='reactor_%s' % config.hostname))
             logger.info('Registred commit_action for {0}'.format(sensor))
-            send_task('reactor.commit_action', [config, sensor], {}, exchange='reactor_%s' % config.hostname)
+            tasks.append(commit_action.subtask((config, sensor), exchange='reactor_%s' % config.hostname))
             results.append('sensor: {0} hostname: {1}, plan: {2}'.format(sensor.get("name"),sensor.get("hostname"), plan_name))
         else:
             results.append('OK - sensor: {0} hostname: {1}, plan: {2}'.format(sensor.get("name"),sensor.get("hostname"), plan_name))
             logger.info('OK - sensor {0} on hostname: {1}, plan {2}'.format(sensor.get("name"),sensor.get("hostname"), plan_name))
-    #job = group(tasks)
-    #result = job.apply_async()
+    job = group(tasks)
+    result = job.apply_async()
     
     return results
