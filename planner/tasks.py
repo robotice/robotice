51511685e1b2e96ac964f39c649d0f62d650d4f5
Michael Kuty <6du1ro.n@gmail.com>
2014-04-25 14:16:50 +0200

    changes

--- planner/tasks.py
+++ planner/tasks.py
@@ -1,5 +1,7 @@
 from yaml import load
 from celery.task import task
+import dateutil.parser
+import datetime
 
 def get_plan(config):
     """pro dany system vrati plan"""
@@ -8,17 +10,23 @@ def get_plan(config):
             if system.get("name") == plan.get("name"):
                 for _plan in config.plans:
                     if _plan.get("name") == system.get("plan"):
-                        return _plan
+                        return system, _plan
     return None
 
 @task(name='planner.get_model_data')
 def get_model_data(config):
-    plan = get_plan(config)
+    system, plan = get_plan(config)
     devices = plan.get('actuators') + plan.get('sensors')
+    start = dateutil.parser.parse(system.get('start'))
+    now = datetime.datetime.now()
+
     for device in devices:
-    	for cycle in device.get('cycles'):
-        	return cycle
-    return devices
+        for cycle in device.get('cycles'):
+            return cycle
+        path = "{0}.{1}.{2}".format(system.get('name'), 'actuator', device.get('name'))
+        time_delta = now - start
+        
+    return time_delta
 
 @task(name='planner.return_model_data')
 def return_model_data(config):
