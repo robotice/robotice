83fb7665d49b3044cd4dc2dfbd1b7ec5d8ab5123
Ales Komarek <mail@newt.cz>
2014-04-25 23:44:42 +0200

    logger

--- reactor/tasks.py
+++ reactor/tasks.py
@@ -2,14 +2,19 @@
 from celery.task import task
  
 from utils import get_grains, import_module
- 
+
 @task(name='reactor.commit_action')
 def commit_action(config, actuator, model_data, real_data):
+
+    logger = commit_action.get_logger()
 
     module_name = ".".join(["reactor", "actuators", actuator.get("device")])
 
     mod = import_module(module_name)
 
     results = mod.run(actuator, model_data, real_data)
+
+    logger.info([actuator, model_data, real_data])
+    logger.info(results)
 
     return results
