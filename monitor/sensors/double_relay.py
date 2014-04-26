2e3b37f61ead5df08c71d21b785cc74c200dfb49
Ales Komarek <mail@newt.cz>
2014-04-26 10:11:06 +0200

    reading values right

--- monitor/sensors/double_relay.py
+++ monitor/sensors/double_relay.py
@@ -6,6 +6,15 @@ def get_data(sensor):
   """
   value = 0
   
+  status_file = '/tmp/robotice_%s' % sensor.get('name')
+
+  try:
+    f = open(status_file, 'w')
+    value = int(f.read())
+    f.close()
+  except:
+    pass
+
   data = []
   data.append(("%s.socket" % sensor.get('name'), value))
       
--- monitor/sensors/relay.py
+++ monitor/sensors/relay.py
@@ -5,7 +5,16 @@ def get_data(sensor):
   relay reading
   """
   value = 0
-  
+
+  status_file = '/tmp/robotice_%s' % sensor.get('name')
+
+  try:
+    f = open(status_file, 'w')
+    value = int(f.read())
+    f.close()
+  except:
+    pass
+
   data = []
   data.append(("%s.socket" % sensor.get('name'), value))
       
