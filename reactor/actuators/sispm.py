b099a7fb422cb82dbf881c22e952afe7b20552fc
Aleš Komárek <mail@newt.cz>
2014-05-08 09:48:02 +0200

    sismpctl

--- reactor/actuators/sispm.py
+++ reactor/actuators/sispm.py
@@ -9,11 +9,10 @@ logger.setLevel(logging.DEBUG)
 
 def run(device, model_data, real_data):
 
-  if device.get('os_family') == 'Debian':
-    executable = "/usr/bin/sispmctl"
-  else:
+  if device.get('os_family') == 'Arch':
     executable = "/usr/local/bin/sispmctl"
-
+  else:
+    executable = "/usr/bin/sispmctl"
 
   if int(model_data) == 0:
     command = [executable, "-d", str(device.get('port')), '-f', str(device.get('socket'))]
