d8671020aa9f3a2a89ac3f1791b626e6880df065
Michael Kuty <6du1ro.n@gmail.com>
2014-09-05 17:36:11 +0200

    logging

--- robotice/monitor/tasks.py
+++ robotice/monitor/tasks.py
@@ -44,6 +44,7 @@ def get_sensor_data(config, sensor, grains):
     LOG.debug("sensor: {0} result: {0}".format(sensor, results))
 
     for result in results:
+
         if isinstance(result[1], (int, long, float, decimal.Decimal)):
 
             result_name = result[0].split('.')[0]
@@ -51,20 +52,27 @@ def get_sensor_data(config, sensor, grains):
 
             system, plan_name = config.get_plan(result_name, result_metric)
             
-            LOG.debug("for result_name: {0} result_metric: {0} was found".format(
+            LOG.info("for result_name: {0} result_metric: {1} system: {2} plan: {3}".format(
                 result_name, 
                 result_metric,
                 system,
                 plan_name))
 
             if system != None:
-                db_key = '%s.%s.%s.%s' % (
-                    system.get('name'), 'sensors', plan_name, 'real')
+                db_key = '.'.join([system.get('name'), 'sensors', sensor.get("name"), 'real'])
                 config.metering.send(db_key, result[1])
                 config.database.set(db_key, result[1])
 
                 LOG.debug("%s: %s" % (db_key, result[1]))
 
                 LOG.debug("metric was sent to database and statsd")
+
+            else:
+
+                LOG.error("System for device %s not found" % result_name)
+
+        else:
+
+            LOG.error("Result from sensor module must be a instance of int, long, float, decimal.Decimal but found %s %s " % (type(result[1]), result[1]))
 
     return 'Started reading real data from sensor %s on device %s at %s' % (sensor, config.hostname, time())
