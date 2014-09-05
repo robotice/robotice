51513c33f3544a42e9990435c4964b83a6a0fa45
Michael Kuty <6du1ro.n@gmail.com>
2014-09-05 21:31:48 +0200

    cleanup

--- robotice/monitor/sensors/hygro.py
+++ robotice/monitor/sensors/hygro.py
@@ -2,16 +2,6 @@ import subprocess
 
 executable = "/srv/robotice/sensors/hygro/driver.py"
 
-def clean_value(value):
-	vals = value.split(",")
-	
-	for v in vals:
-		try:
-			r = int(v)
-		except Exception, e:
-			r = value
-	return r
-
 def get_data(sensor):
 
     port = str(sensor.get('port'))
@@ -25,6 +15,6 @@ def get_data(sensor):
 
     metric_format = "{0}.hygro_{1}"
 
-    data.append((metric_format.format(sensor.get('name'), sensor.get("type").lower()), clean_value(value),))
+    data.append((metric_format.format(sensor.get('name'), sensor.get("type").lower()), value,))
 
     return data
