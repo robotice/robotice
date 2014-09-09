e600ce50febd1a4ff32ca6bd91c6eeb7cf3f197f
Michael Kuty <6du1ro.n@gmail.com>
2014-09-09 12:16:19 +0200

    fix

--- robotice/worker_monitor.py
+++ robotice/worker_monitor.py
@@ -15,7 +15,7 @@ BROKER_URL = config.broker
 if "rabbitmq" in config.broker:
 
     default_exchange = Exchange('default', type='fanout')
-    monitor_exchange = Exchange('monitor_%s' % config.hostname, type='topic')
+    monitor_exchange = Exchange('monitor_%s' % config.hostname, type='fanout')
     reactor_exchange = Exchange('reactor', type='fanout')
     planner_exchange = Exchange('planner', type='fanout')
 
@@ -60,7 +60,7 @@ CELERY_ROUTES = {
         'queue': 'reasoner',
     },
     'monitor.get_sensor_data': {
-        'queue': 'monitor_%s' % config.hostname,
+        'queue': 'monitor',
     },
     'monitor.return_sensor_data': {
         'queue': 'monitor',
