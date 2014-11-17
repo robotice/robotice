08991758e781520f52d04eeb5585ef099a114b8b
Michael Kuty <6du1ro.n@gmail.com>
2014-11-17 23:08:51 +0100

    subprocess util

--- robotice/utils/__init__.py
+++ robotice/utils/__init__.py
@@ -2,9 +2,11 @@
 from robotice.utils.database import PickledRedis
 from robotice.utils.decorators import norecursion
 from robotice.utils.dictionary import dict_merge
+from robotice.utils.subprocess import call_command
 
 __all__ = [
     "PickledRedis",
     "norecursion",
     "dict_merge",
+    "call_command",
 ]
---
+++ robotice/utils/subprocess.py
@@ -0,0 +1,24 @@
+
+from __future__ import absolute_import
+
+import logging
+
+from subprocess import Popen, PIPE
+
+logger = logging.getLogger(__name__)
+
+def call_command(cmd, verbosity="ERROR"):
+  """
+  simple wrapper for call command
+  """
+
+  p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+  output, err = p.communicate("get output")
+
+  rc = p.returncode
+
+  if not rc == 0:
+    raise Exception(err)
+
+  return output
