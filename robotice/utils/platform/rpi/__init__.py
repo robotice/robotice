43a8cb0901387adf25eb76288ea2dd0ae63f5348
Michael Kuty <6du1ro.n@gmail.com>
2014-11-17 16:36:22 +0100

    noqa

---
+++ robotice/utils/platform/rpi/__init__.py
@@ -0,0 +1,9 @@
+
+from robotice.utils.platform.rpi.const import RPI_PIN_2_GPIO_MAP
+from robotice.utils.platform.rpi.const import RPI_PIN_2_GPIO_MAP
+
+
+__all__ = [
+    "RPI_PIN_2_GPIO_MAP",
+    "RPI_PIN_2_GPIO_MAP",
+]
