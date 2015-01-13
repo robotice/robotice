24bcc1db04a03023a48980330d3313764d765a61
Michael Kutý <6du1ro.n@gmail.com>
2015-01-13 22:48:42 +0100

    provide celery app to managers

--- robotice/conf/managers/base.py
+++ robotice/conf/managers/base.py
@@ -11,6 +11,11 @@ import logging
 
 from robotice.common import importutils
 
+from celery import states
+from celery import Celery
+from celery.result import AsyncResult
+from celery.backends.base import DisabledBackend
+
 LOG = logging.getLogger(__name__)
 
 
@@ -28,6 +33,17 @@ class BaseManager(object):
 
         return conf_mod.setup_app(role)
 
+    def capp(self, name=None, default="reasoner"):
+        """celery app
+        """
+        if default and name is None:
+            mod = self.find_some_worker()
+            return self.load_app(mod)
+
+        conf = importutils.import_module("robotice.worker_%s" % name)
+
+        return self.load_app(conf)
+
     def list(self, id):
         raise NotImplementedError
 
@@ -36,3 +52,27 @@ class BaseManager(object):
 
     def get(self, id):
         raise NotImplementedError
+
+    @staticmethod
+    def load_app(module):
+        """load celery app from config file
+        """
+
+        app = Celery(module.__name__)
+        app.config_from_object(module)
+
+        return app
+
+    def find_some_worker(self):
+        """ this method find first available worker
+        """
+
+        for worker in ["reasoner", "planer", "monitor", "reactor"]:
+
+            try:
+                conf = importutils.import_module("robotice.worker_%s" % worker)
+                return conf
+            except ImportError:
+                raise e
+
+        return None
