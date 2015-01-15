1dc5f99f3ea25fc4bdb79860d32c1e25cf5369cf
Michael Kutý <6du1ro.n@gmail.com>
2015-01-15 21:54:39 +0100

    fix init

--- robotice/conf/__init__.py
+++ robotice/conf/__init__.py
@@ -1,10 +1,12 @@
 
 from robotice.conf.conf import RoboticeSettings
 from robotice.conf.conf import Settings
+from robotice.conf.conf import settings
 from robotice.conf.conf import setup_app
 
 __all__ = [
     "Settings",
     "RoboticeSettings",
     "setup_app",
+    "settings",
 ]
