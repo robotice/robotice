3c53a907b6a3fdf6e2a1eda7f2d26af1150e194b
Michael Kutý <6du1ro.n@gmail.com>
2015-02-26 22:45:19 +0100

    new consistent releasing

--- robotice/__init__.py
+++ robotice/__init__.py
@@ -1,23 +1,6 @@
-__title__ = 'Robotice'
-__release__ = '1'
-__version__ = '0.2.6'
-__author__ = 'Ales Komarek & Michael Kuty'
-__license__ = """
-Copyright 2014 Ales Komarek & Michael Kuty
+import pbr.version
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-__copyright__ = ''
+__version__ = pbr.version.VersionInfo('robotice').version_string()
 
 ROBOTICE_BANNER="""
  ______       _                _             
--- setup.py
+++ setup.py
@@ -1,88 +1,14 @@
-#!/usr/bin/env python
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-from pip.req import parse_requirements
-from codecs import open  # To use a consistent encoding
-from os import path
-import sys
-import os
+import setuptools
 
-here = path.abspath(path.dirname(__file__))
-
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
 try:
-    from setuptools import setup
+    import multiprocessing  # noqa
 except ImportError:
-    from distutils.core import setup
+    pass
 
-import robotice
-
-PACKAGE_NAME = 'robotice'
-PACKAGE_DIR = 'robotice'
-extra = {}
-
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
-
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    readme = f.read()
-"""
-with open(path.join(here, 'HISTORY.rst'), encoding='utf-8') as f:
-    history = f.read()
-with open(path.join(here, 'LICENSE'), encoding='utf-8') as f:
-    license = f.read()
-"""
-
-install_requires = """
-python-statsd==1.6.0
-PyYAML>=3.10
-celery>=3.1.7
-flower==0.6.0
-pytz==2011k
-redis>=2.10.3
-python-dateutil==2.2
-raven>=5.0.0
-celery-with-redis==3.0
-prettytable==0.7.2
-oslo.config==1.4.0
-anyconfig==0.0.5
-""".split()
-
-
-setup(
-    author='Ales Komarek & Michael Kuty',
-    author_email='mail@newt.cz & mail@majklk.cz',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Topic :: System :: Software Distribution',
-        'Intended Audience :: Education',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Operating System :: OS Independent',
-    ],
-    description='Opensource monitoring, reasoning and acting framework.',
-    include_package_data=True,
-    install_requires=install_requires,
-    packages=find_packages(exclude=['docs', 'tests*']),
-    package_dir={
-        PACKAGE_NAME: PACKAGE_NAME,
-    },
-    license=robotice.__license__,
-    long_description=readme,
-    name=PACKAGE_NAME,
-    platforms=['any'],
-    url='https://github.com/robotice/robotice',
-    version=robotice.__version__,
-    zip_safe=False,
-    entry_points={
-        'console_scripts': [
-            'robotice=robotice.bin.robotice:main',
-        ],
-    },
-    **extra)
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
